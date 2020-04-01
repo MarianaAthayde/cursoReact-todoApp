5f80062c5ec59a954d4cf777865799abcbd8b92f
AXXIOM1\mgarcia <mariana.garcia@axxiom.com.br>
2020-03-31 22:05:51 -0300

    -Criação das rotas; -Criação do Menu; -Componente de Page Header; -Conteúdo about.

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
 import 'modules/font-awesome/css/font-awesome.min.css';
-import React from 'react';
+import Menu from '../template/menu';
+import Routes from './routes';
 
 export default props => (
     <div className='container'>
-        <h1>Teste ToDo App</h1>
+        <Menu />
+        <Routes />
     </div>
 );
