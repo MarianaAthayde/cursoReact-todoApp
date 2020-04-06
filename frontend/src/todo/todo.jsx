a318646d709d2869360ce9993ea9f133ca5a6201
AXXIOM1\mgarcia <mariana.garcia@axxiom.com.br>
2020-04-06 17:38:33 -0300

    Frontend: -Implementação da lista de tarefas -Pesquisa -Remover tarefas -Marcar como concluída ou pendente -Atalhos de teclado

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,15 +1,99 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+const URL = 'http://localhost:3003/api/todos';
+
 export default class Todo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            description: '',
+            list: []
+        }
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+        this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.refresh();
+    }
+
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : '';
+        axios.get(`${URL}?sort=createdAt${search}`)
+            .then(resp => this.setState({ ...this.state, description: description, list: resp.data }));
+    }
+
+    handleChange(e) {
+        this.setState({ ...this.state, description: e.target.value });
+    }
+
+    handleAdd() {
+        const description = this.state.description
+        axios.post(URL, { description })
+            .then(resp => {
+                alert('ToDo adicionado com sucesso!');
+                this.refresh();
+            })
+            .catch(error => console.log('Erro ao tentar adicionar tarefas!'));
+    }
+
+    handleRemove(item) {
+        axios.delete(`${URL}/${item._id}`)
+            .then(resp => {
+                alert('Item removido com sucesso!');
+                this.refresh();
+            })
+            .catch(erro => {
+                alert('Ocorreu um erro ao remover o item!');
+                console.log(erro);
+            })
+    }
+
+    handleMarkAsDone(item) {
+        axios.put(`${URL}/${item._id}`, { ...item, done: true })
+            .then(resp => {
+                alert('Item atualizado com sucesso!');
+                this.refresh(this.state.description);
+            })
+            .catch(erro => {
+                alert('Ocorreu um erro ao atualizar o item!');
+                console.log(erro);
+            });
+    }
+
+    handleMarkAsPending(item) {
+        axios.put(`${URL}/${item._id}`, { ...item, done: false })
+            .then(resp => {
+                alert('Item atualizado com sucesso!');
+                this.refresh(this.state.description);
+            })
+            .catch(erro => {
+                alert('Ocorreu um erro ao atualizar o item!');
+                console.log(erro);
+            });
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description);
+    }
+
     render() {
         return (
             <div>
                 <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
-                <TodoForm></TodoForm>
-                <TodoList></TodoList>
+                <TodoForm description={this.state.description}
+                    handleAdd={this.handleAdd}
+                    handleChange={this.handleChange}
+                    handleSearch={this.handleSearch}></TodoForm>
+                <TodoList list={this.state.list}
+                    handleRemove={this.handleRemove}
+                    handleMarkAsDone={this.handleMarkAsDone}
+                    handleMarkAsPending={this.handleMarkAsPending}></TodoList>
             </div>
         )
     }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
+import IconButton from '../template/iconButton';
 
-export default props => (
-    <div>
-        <h1>ToDo List</h1>
-    </div>
-);
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || [];
+
+        return list.map(item => (
+            <tr key={item._id}>
+                <td>
+                    {item.description}
+                </td>
+                <td>
+                    {JSON.stringify(item.done)}
+                </td>
+                <td>
+                    <IconButton style='success' icon='check' hide={item.done}
+                        onClick={() => props.handleMarkAsDone(item)}>
+                    </IconButton>
+                    <IconButton style='warning' icon='undo' hide={!item.done}
+                        onClick={() => props.handleMarkAsPending(item)}>
+                    </IconButton>
+                    <IconButton style='danger' icon='trash-o'
+                        onClick={() => props.handleRemove(item)}>
+                    </IconButton>
+                </td>
+            </tr>
+        ));
+    };
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>
+                        Descrição
+                    </th>
+                    <th>
+                        Status
+                    </th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    );
+};
