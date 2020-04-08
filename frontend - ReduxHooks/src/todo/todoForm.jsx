e79a3ae22fb8bb5e86b7c28359da73d297278560
AXXIOM1\mgarcia <mariana.garcia@axxiom.com.br>
2020-04-08 12:43:53 -0300

    Frontend: -Adaptando as ações para utilizar hooks (useDispatch)

--- frontend - ReduxHooks/src/todo/todoForm.jsx
+++ frontend - ReduxHooks/src/todo/todoForm.jsx
@@ -1,27 +1,18 @@
 import React, { Component, useEffect } from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { connect, useSelector } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { handleChange, updateList, add } from '../actions/todoAction';
 
 const URL = 'http://localhost:3003/api/todos';
 
-function TodoForm(props) {
+export default function TodoForm(props) {
     // constructor(props) {
     //     super(props);
 
     //     this.keyHandle = this.keyHandle.bind(this);
     // }
-
-    useEffect(() => {
-        props.updateList();
-        return;
-    },
-        []
-    );
-
-    const description = useSelector(state => state.todo.description);
 
     // keyHandle(e) {
     //     switch (e.key) {
@@ -29,25 +20,41 @@ function TodoForm(props) {
     //     }
     // };
 
+    const description = useSelector(state => state.todo.description);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        updateListF();
+        return;
+    },
+        []
+    );
+
+    function handleChangeF(value) {
+        dispatch(handleChange(value));
+    }
+
+    function addF(description) {
+        dispatch(add(description));
+    }
+
+    function updateListF() {
+        dispatch(updateList());
+    }
+
     return (
         <div role='form' className='todoForm' >
             <Grid cols='12 9 10'>
                 <input id='description' className='form-control' placeholder='Adicione uma tarefa'
                     value={description}
-                    onChange={(event) => props.handleChange(event.target.value)}
+                    onChange={(event) => handleChangeF(event.target.value)}
                 // onKeyUp={this.keyHandle}
                 ></input>
             </Grid>
             <Grid cols='12 3 2'>
-                <IconButton style='primary' icon='plus' onClick={() => props.add(description)}></IconButton>
-                <IconButton style='info' icon='search' onClick={props.updateList}></IconButton>
+                <IconButton style='primary' icon='plus' onClick={() => addF(description)}></IconButton>
+                <IconButton style='info' icon='search' onClick={() => updateListF()}></IconButton>
             </Grid>
         </div>
     )
 };
-
-function mapDispatchToProps(dispatch) {
-    return bindActionCreators({ handleChange, updateList, add }, dispatch);
-}
-
-export default connect(null, mapDispatchToProps)(TodoForm)
